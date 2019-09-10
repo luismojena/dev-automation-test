19984259e88ce0ba836554237aa7d2a39e7967c9
lmojenaroman <luis.mojena-roman@alayacare.com>
2019-09-10 19:16:38 -0400

    fifth step. Done

---
+++ init_database.groovy
@@ -0,0 +1,12 @@
+pipeline {
+    agent any
+    stages {
+        stage('Init DB') {
+            steps {
+                sh '''
+                   mysql -h db -u root -p$MYSQL_ROOT_PASSWORD < /data/database.sql
+                '''
+            }
+        }
+    }
+}
